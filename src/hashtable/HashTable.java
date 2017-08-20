3dcc2e276138d6e624f9af63744398062de86ff1
va64doman <va64doman@hotmail.com>
2017-08-20 21:26:01 +0800

    Add Gitignore

---
+++ src/hashtable/HashTable.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashtable;
+
+/**
+ *
+ * @author Van Do
+ */
+public class HashTable 
+{
+    // Hash table's size
+    private int tSize;
+    // Size of elements in table
+    private int size;
+    // Array of buckets in table
+    private StudentEntry[] table;
+    // Prime number
+    private int primeSize;
+}
---
+++ src/hashtable/StudentEntry.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashtable;
+
+/**
+ *
+ * @author Van Do
+ */
+
+// The student has details to contain as an existence to the object
+public class StudentEntry 
+{
+    // Student's ID
+    // The id is going to be the hash table value
+    private int id;
+    // Stundent's name
+    // The name is going to be the hash table key
+    private String name;
+    // Student's age
+    private int age;
+    // Student's subject of choice
+    private String subject;
+    // Next student in a hash table after this student
+    private StudentEntry next;
+    // Parameterised constructor to initialise object
+    public StudentEntry(int sId, String sName, int sAge, String sSubject)
+    {
+        id = sId;
+        name = sName;
+        age = sAge;
+        subject = sSubject;
+        next = null;
+    }
+    // Getters
+    // Get student's id
+    public int getID()
+    {
+        return id;
+    }
+    // Get student's name
+    public String getName()
+    {
+        return name;
+    }
+    // Get student's age
+    public int getAge()
+    {
+        return age;
+    }
+    // Get student's subject
+    public String getSubject()
+    {
+        return subject;
+    }
+    // Get next chained student after this student
+    public StudentEntry getNext()
+    {
+        return next;
+    }
+    // Setters
+    // Set new name
+    public void setName(String sName)
+    {
+        name = sName;
+    }
+    // Set new age
+    public void setAge(int sAge)
+    {
+        age = sAge;
+    }
+    // Set new subject
+    public void setSubject(String sSubject)
+    {
+        subject = sSubject;
+    }
+    // Set new key-value pairs at the end of the list
+    public void setNext(StudentEntry student)
+    {
+        next = student;
+    }
+    // Print details
+    public void printDetails()
+    {
+        System.out.println("Student's ID: " + id);
+        System.out.println("Student's name: " + name);
+        System.out.println("Student's age: " + age);
+        System.out.println("Student's subject: " + subject);
+    }
+}
